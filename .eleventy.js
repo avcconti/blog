37c6da71f2fa46afe1db9f4b06a0ed90982d8751
ARC Team <dev@producthackers.com>
2022-12-01 17:11:46 +0100

    feat: added local fonts

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = (function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("fonts");
 
   // Return the smallest number argument
   eleventyConfig.addFilter("min", (...numbers) => {
