dcc22ec7e3175224d8522df5e0225beb50145e04
Andrés Velasco <avc.conti@gmail.com>
2022-12-27 12:28:27 +0100

    feat: added MVP

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+
+
+module.exports = (function(eleventyConfig) {
+  // Copy the `img` and `css` folders to the output
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+
+});
